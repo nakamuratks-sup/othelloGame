f8f596e033c2a0cf915afac421b5296eac50b00b
T-nakamura0609 <nkmr.tk4.2525.com@gmail.com>
2021-05-21 14:03:26 +0900

    関西ハンズオン講習用の元ネタの元ネタ

---
+++ othelloGame/src/main/java/jp/co/supply_net/game/othello/controller/DemoController.java
@@ -0,0 +1,29 @@
+package jp.co.supply_net.game.othello.controller;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+public class DemoController {
+	private static final Logger log = LoggerFactory.getLogger( DemoController.class);
+
+	@RequestMapping(path = "/demo")
+	public String demoMethodPre(Model model) {
+		return "demoview";
+
+	}
+
+	@RequestMapping(path = "/demo", method = RequestMethod.GET)
+	public String demoMethodSeond(Model model) {
+		model.addAttribute("timestamp", new Date().toString());
+		return "demoview";
+	}
+}
---
+++ othelloGame/src/main/java/jp/co/supply_net/game/othello/dto/DemoDto.java
@@ -0,0 +1,11 @@
+package jp.co.supply_net.game.othello.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DemoDto {
+	private int xpos;
+	private int ypos;
+}
