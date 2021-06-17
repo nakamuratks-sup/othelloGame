801903f30eefd95b22e82f215ded1fd052967d4d
T-nakamura0609 <nkmr.tk4.2525.com@gmail.com>
2021-06-17 13:43:08 +0900

    json受け取りサンプル追加

--- othelloGame/src/main/resources/static/js/demoview.js
+++ othelloGame/src/main/resources/static/js/demoview.js
@@ -24,7 +24,7 @@ function clickMe(e) {
 
     postmsg(this.id);
     // document.getElementById("tmstmp").textContent = myNameSpace.rcvdata;
-
+    jsonPost();
 };
 
 function postmsg(id) {
@@ -46,6 +46,32 @@ function postmsg(id) {
             // 設定先のidを取得して値を設定
             var tmp2 = $('#tmstmp');
             tmp2.text(tmp.text());
-        })
-    
-}
+        });
+};
+
+// サーバへjsonで送るサンプル
+function jsonPost(){
+    $("#jsr1").text("");
+    $("#jsr1").text("");
+
+    var json1 = {
+        bangou: "1",
+        name: "山田",
+    };
+
+    $.ajax({
+        url: "/sample/json",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(json1),
+        dataType: "json",
+    })
+    .done(function (date1, textStatus, jqXHR) {
+        $("#jsr1").text(jqXHR.status);
+        $("#jsr2").text(JSON.stringify(date1));
+    })
+    .fail(function (jqXHR, textStatus, errorThrown) {
+        $("#jsr1").text(jqXHR.status);
+    })
+    .always(function () { }) ;
+};
