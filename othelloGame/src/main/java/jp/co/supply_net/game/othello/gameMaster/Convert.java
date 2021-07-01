f826cbef3bb88bbf29f421c3fd3ba5006b086624
T-nakamura0609 <nkmr.tk4.2525.com@gmail.com>
2021-07-02 00:46:25 +0900

    逆変換のメソッドのガワ追加

--- othelloGame/src/main/java/jp/co/supply_net/game/othello/gameMaster/Convert.java
+++ othelloGame/src/main/java/jp/co/supply_net/game/othello/gameMaster/Convert.java
@@ -8,6 +8,7 @@ import jp.co.supply_net.game.othello.dto.BoardInfo.Masu;
 
 public class Convert {
 
+	// フロント->サーバ変換
 	private int convertNumX(String number){
         int num = Integer.parseInt(number);
         int num_x = num % 8 - 1;
@@ -22,19 +23,22 @@ public class Convert {
         return num_y;
     }
 
-    public Grid convertNum(Masu masu) {
+    public Grid convertNum(String number, String masuJoho) {
     	Grid grid = new Grid();
-    	grid.setXPosition(this.convertNumX(masu.getNumber()));
-    	grid.setYPosition(this.convertNumY(masu.getNumber()));
-        grid.setSType(this.changType(masu.getMasuJoho()));
+    	grid.setXPosition(this.convertNumX(number));
+    	grid.setYPosition(this.convertNumY(number));
+        grid.setSType(this.changType(masuJoho));
     	return grid;
 	}
+    public Grid convertNum(Masu masu) {
+    	return this.convertNum(masu.getNumber(), masu.getMasuJoho());
+    }
 
     public List<Grid> convertGridList(List<Masu> boardlist){
     	List<Grid> list = new ArrayList<>();
 
     	for(Masu masu: boardlist) {
-    		Grid grid = this.convertNum(masu);
+    		Grid grid = this.convertNum(masu.getNumber(), masu.getMasuJoho());
     		list.add(grid);
     	}
     	return list;
@@ -45,4 +49,30 @@ public class Convert {
         return null;
     }
 
+    // サーバ->フロント変換
+
+    /**
+     * GridリストをMasuリストに変換する
+     * @param gridlist
+     * @return
+     */
+    public List<Masu> convertMasuList(List<Grid> gridlist){
+    	List<Masu> list = new ArrayList<>();
+
+    	for(Grid grid: gridlist) {
+    		Masu masu = convertMasu(grid);
+    		list.add(masu);
+    	}
+    	return list;
+    }
+
+    /**
+     * GridをMasuに変換する
+     * @param grid
+     * @return
+     */
+    public Masu convertMasu(Grid grid) {
+    	// Gridのx,yからMasuの通し番号に変換する
+    	return null;
+    }
 }
